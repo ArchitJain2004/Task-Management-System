7c5e378e76ac82322c40c6af8ede749b847b07d8
ArchitJain2004 <architjain.udr1@gmail.com>
2025-11-03 21:09:53 +0530

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "http://3.87.116.104:8800",
         changeOrigin: true,
       },
     },
